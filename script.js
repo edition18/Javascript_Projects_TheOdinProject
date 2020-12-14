dc8f0e35ae0a535fb8a735de24fbe8cbea78af96
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-12-14 12:02:58 +0800

    further updates

--- script.js
+++ script.js
@@ -1,68 +1,65 @@
-document.getElementById("addBookBtn").addEventListener("click", formSubmit); 
-
+document.getElementById("addBookBtn").addEventListener("click", formSubmit);
 
 let myLibrary = [];
 
-function Book(title, author, pages,read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
+Book.prototype.saytitle = function () {
+  console.log(this.title);
+};
 
-Book.prototype.saytitle = function() {
-  console.log(this.title)
+function addBookToLibrary(title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read));
 }
 
-function addBookToLibrary(title, author, pages,read) {
-  myLibrary.push(new Book(title, author, pages,read))
-}
-
-addBookToLibrary("test","x",12,false)
-addBookToLibrary("test2","y",1231,false)
-addBookToLibrary("test3","z",2313,false)
+addBookToLibrary("test", "x", 12, false);
+addBookToLibrary("test2", "y", 1231, false);
+addBookToLibrary("test3", "z", 2313, false);
 
 function displayBooks() {
   let target = document.getElementById("displayArea");
-  target.innerHTML = "" // clear on reclick
+  target.innerHTML = ""; // clear on reclick
   myLibrary.forEach((item) => {
     let subPara = document.createElement("div");
-    subPara.setAttribute("id", "testid")
-    let title = `<h2>Title: ${item.title}</h1>`
-    let author = `<h3>Author: ${item.author}</h2>`
-    let pages = `<h5>Pages: ${item.pages}</h4>`
-    let read = `<h5>read: ${item.read}</h>`
+    subPara.setAttribute("id", "testid");
+    let title = `<h2>Title: ${item.title}</h1>`;
+    let author = `<h3>Author: ${item.author}</h2>`;
+    let pages = `<h5>Pages: ${item.pages}</h4>`;
+    let read = `<h5>read: ${item.read}</h>`;
     // use indexOf to access index
-    let bookId = `<h5>index: ${myLibrary.indexOf(item)}</h5><button onclick="deleteBook(this)"></button>`
+    let bookId = `<h5>index: ${myLibrary.indexOf(
+      item
+    )}<button onclick="deleteBook(this)">TEST</button>`;
     // get value of the element triggering the button
     subPara.innerHTML = `${title}${author}${pages}${read}${bookId}`;
-    target.append(subPara)
-  })
-  
+    target.append(subPara);
+  });
 }
 
-
 function deleteBook(element) {
-  console.log(element)
+  console.log(element);
 }
 
 function displayForm() {
-  var node = document.getElementById('bookForm');
+  var node = document.getElementById("bookForm");
   var visibility = node.style.visibility;
-  node.style.visibility = visibility == "visible" ? 'hidden' : "visible"
+  node.style.visibility = visibility == "visible" ? "hidden" : "visible";
 }
 
 function formSubmit() {
-  // this will be able to reference the formID 
-  title = bookForm.title.value; 
-  author = bookForm.author.value; 
-  pages = bookForm.pages.value; 
-  read = bookForm.read.checked; 
+  // this will be able to reference the formID
+  title = bookForm.title.value;
+  author = bookForm.author.value;
+  pages = bookForm.pages.value;
+  read = bookForm.read.checked;
 
-  addBookToLibrary(title, author, pages,read)
-  displayBooks()
+  addBookToLibrary(title, author, pages, read);
+  displayBooks();
 }
 
-
-console.log(myLibrary)
+console.log(myLibrary);
